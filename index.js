fd0dbae3b95593f9667eaf65f3a34fa3132c6322
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-18 21:31:26 +0200

    Verified by Marti4n v1.0.2 50% done

--- index.js
+++ index.js
@@ -1,16 +1,195 @@
-const body    = document.querySelector("body")
-const section = document.createElement("section")
-const div     = document.createElement("div")
-const label   = document.createElement("label")
-const input   = document.createElement("input")
-const button  = document.createElement("button")
-const span    = document.createElement("span")
+const mediaQueryMobile  = window.matchMedia('(max-width: 375px)');
+const body              = document.querySelector("body")
+const section           = document.createElement("section")
+const div               = document.createElement("div")
+const label             = document.createElement("label")
+const input             = document.createElement("input")
+const button            = document.createElement("button")
+const span              = document.createElement("span")
+
+/*
+.########...#######..########..##....##
+.##.....##.##.....##.##.....##..##..##.
+.##.....##.##.....##.##.....##...####..
+.########..##.....##.##.....##....##...
+.##.....##.##.....##.##.....##....##...
+.##.....##.##.....##.##.....##....##...
+.########...#######..########.....##...
+*/
+
+body.style.margin        = "0"
+body.style.height        = "100vh"
+body.style.boxSizing     = "border-box"
+body.style.fontFamily    = "'Roboto', sans-serif"
 
 
 
-label.textContent = "To stay up-to-date with important releases, Subscribe!"
+
+/*
+.##..........###....########..########.##......
+.##.........##.##...##.....##.##.......##......
+.##........##...##..##.....##.##.......##......
+.##.......##.....##.########..######...##......
+.##.......#########.##.....##.##.......##......
+.##.......##.....##.##.....##.##.......##......
+.########.##.....##.########..########.########
+*/
+label.style.fontSize    = "20px"
+label.style.color       = "black"
+label.style.padding     = "60px 0 0 0"
+label.style.fontWeight  = "bolder"
 
 
+
+
+/*
+..######..########..######..########.####..#######..##....##
+.##....##.##.......##....##....##.....##..##.....##.###...##
+.##.......##.......##..........##.....##..##.....##.####..##
+..######..######...##..........##.....##..##.....##.##.##.##
+.......##.##.......##..........##.....##..##.....##.##..####
+.##....##.##.......##....##....##.....##..##.....##.##...###
+..######..########..######.....##....####..#######..##....##
+*/
+//section.style.display    = "none"
+section.style.background  = "gray"
+section.style.textAlign   = "center"
+section.style.width       = "100%"
+section.style.height      = "100%"
+section.style.position    = "absolute"
+section.style.top         = "0"
+section.style.left        = "0"
+
+
+
+
+
+/*
+.########..####.##.....##
+.##.....##..##..##.....##
+.##.....##..##..##.....##
+.##.....##..##..##.....##
+.##.....##..##...##...##.
+.##.....##..##....##.##..
+.########..####....###...
+*/
+div.style.width           = "50%"
+div.style.height          = "25vw"
+div.style.background      = "orange"
+div.style.display         = "flex"
+div.style.flexDirection   = "column"
+div.style.alignItems      = "center"
+div.style.justifyContent  = "space-around"
+div.style.position        = "relative"
+div.style.margin          = "50px auto"
+  
+
+
+
+
+/*
+.##..........###....########..########.##......
+.##.........##.##...##.....##.##.......##......
+.##........##...##..##.....##.##.......##......
+.##.......##.....##.########..######...##......
+.##.......#########.##.....##.##.......##......
+.##.......##.....##.##.....##.##.......##......
+.########.##.....##.########..########.########
+*/
+label.textContent       = "To stay up-to-date with important releases, Subscribe!"
+label.style.fontSize    = "20px"
+label.style.color       = "black"
+label.style.padding     = "60px 0 0 0"
+label.style.fontWeight  = "bolder"
+
+
+  
+/*
+.####.##....##.########..##.....##.########
+..##..###...##.##.....##.##.....##....##...
+..##..####..##.##.....##.##.....##....##...
+..##..##.##.##.########..##.....##....##...
+..##..##..####.##........##.....##....##...
+..##..##...###.##........##.....##....##...
+.####.##....##.##.........#######.....##...
+*/
+input.type                 = "email"
+input.placeholder          = "youremail@example.com"
+input.style.fontSize       = "30px"
+input.style.padding        = "18px"
+input.style.width          = "80%"
+input.style.textAlign      = "center"
+input.style.borderRadius   = "10px"
+
+
+
+/*
+.########..##.....##.########.########..#######..##....##
+.##.....##.##.....##....##.......##....##.....##.###...##
+.##.....##.##.....##....##.......##....##.....##.####..##
+.########..##.....##....##.......##....##.....##.##.##.##
+.##.....##.##.....##....##.......##....##.....##.##..####
+.##.....##.##.....##....##.......##....##.....##.##...###
+.########...#######.....##.......##.....#######..##....##
+*/
+button.textContent         = "Submit"
+button.style.fontSize      = "30px"
+button.style.padding       = "10px"
+button.style.background    = "red"
+button.style.color         = "white"
+button.style.fontWeight    = "bolder"
+button.style.borderRadius  = "10px"
+button.style.border        = "none"
+
+
+/*
+..######..########.....###....##....##
+.##....##.##.....##...##.##...###...##
+.##.......##.....##..##...##..####..##
+..######..########..##.....##.##.##.##
+.......##.##........#########.##..####
+.##....##.##........##.....##.##...###
+..######..##........##.....##.##....##
+*/
+span.textContent       = "x"
+span.style.position    = "absolute"
+span.style.top         = "18px"
+span.style.right       = "18px"
+span.style.fontSize    = "20px"
+span.style.fontWeight  = "bolder"
+//?????????
+if(span.matches(":hover")) {
+    span.style.cursor = "pointer"
+}
+
+
+/*
+.##.....##.########.########..####....###........#######..##.....##.########.########..##....##
+.###...###.##.......##.....##..##....##.##......##.....##.##.....##.##.......##.....##..##..##.
+.####.####.##.......##.....##..##...##...##.....##.....##.##.....##.##.......##.....##...####..
+.##.###.##.######...##.....##..##..##.....##....##.....##.##.....##.######...########.....##...
+.##.....##.##.......##.....##..##..#########....##..##.##.##.....##.##.......##...##......##...
+.##.....##.##.......##.....##..##..##.....##....##....##..##.....##.##.......##....##.....##...
+.##.....##.########.########..####.##.....##.....#####.##..#######..########.##.....##....##...
+*/
+if(mediaQueryMobile) {
+
+    body.style.backgroundColor =  "black"
+    body.style.margin          = "0"
+    body.style.height          = "100vh"
+    body.style.boxSizing       = "border-box"
+    body.style.fontFamily      = "'Roboto', sans-serif"
+
+    div.style.width            = "90%"
+    div.style.height           = "90vw"
+    div.style.background       = "orange"
+    div.style.display          = "flex"
+    div.style.flexDirection    = "column"
+    div.style.alignItems       = "center"
+    div.style.justifyContent   = "space-around"
+    div.style.position         = "relative"
+    div.style.margin           = "50px auto"
+} 
 
 
 div.appendChild(label)
